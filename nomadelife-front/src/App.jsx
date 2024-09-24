bad9427b79ff0821573a6074378ef45c75918e46
MateusLGoncalves <mateuslgoncalves@outlook.com>
2024-09-24 11:56:59 -0300

    Aula0924 - Hook de Authentication

--- nomadelife-front/src/App.jsx
+++ nomadelife-front/src/App.jsx
@@ -3,10 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
-<<<<<<< HEAD
 import Footer from './components/Footer'
-=======
->>>>>>> 66063a444613acc6531724a2b3f5634026382961
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,11 +11,8 @@ function App() {
   return (
     <>
       <NavBar />
-<<<<<<< HEAD
 
       <Footer />
-=======
->>>>>>> 66063a444613acc6531724a2b3f5634026382961
     </>
   )
 }
--- nomadelife-front/src/hooks/useAuthentication.jsx
+++ nomadelife-front/src/hooks/useAuthentication.jsx
@@ -85,6 +85,6 @@ export const useAuthentication = () => {
             loading,
             logout,
             login
-        )
+        )   
     }
 }
